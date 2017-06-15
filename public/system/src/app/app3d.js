2bdcb1a6b507d09f49bee3c2187c3403453b69d2
Samuel <pudge5515@gmail.com>
2017-06-15 10:57:38 +0200

    add tinymce and greensock plugin

--- public/system/src/app/app3d.js
+++ public/system/src/app/app3d.js
@@ -2,163 +2,163 @@
 var scene = require('../core/scene.js'),
     __s__ = require('../utils/js-helpers.js'),
     __d__ = require('../utils/dom-utilities.js'),
-    mgmInfoMod = require('./ui/mgmInfo.js'),
+    mgmInfoMod = require('./ui-admin/mgmInfo.js'),
+    rightClickMod = require('./ui-admin/rightClick.js'),
     winInfoMod = require('./ui/winInfo.js'),
-    viewsNavMod = require('./ui/viewsNav.js'),
+    mainNavigatorMod = require('./ui/mainNavigator.js'),
     markersNavMod = require('./ui/markersNav.js'),
+    mediaNavMod = require('./ui/mediaNav.js'),
+    appTracker = require('../mgmt/actions/tracker.js'),
+    groupsCommandsMod = require('./ui/mods/groupsCmds.js'),
     appReactActionsWelcome = require('../mgmt/actions/welcome.js');
 
 window.__reactApp = {};
 window.opxl = window.opxl || {};
+
+const prefixMarker = markersNavMod.prefixMarker;
+const orderLowercase = (a, b) => { return String(a).toLowerCase() > String(b).toLowerCase() ? 1 : -1; }
 
 var scope = this,
     node = document.getElementById("app-3d"),
     app3d = new scene.Scene(node),
     INTERSECTED = null,
     viewsNav,
+    mainNavigator,
+    localNavigator,
     winInfo,
+    winPopup,
     mgmInfo,
-    markersNav;
+    rightClickNav,
+    markersNav,
+    gpsCmds,
+    mediaNav;
 
 function onHover(ev) {
     
     let intersected = ev.intersected,
-        infoMap = window.config.infoMap;
+        obj;
+    if (!intersected) { return; }
 
-    if (winInfo._isPersistant) { return; }
+    //Check if it's a marker
+    if (mgmInfo.layv) {
+        let rcContext = rightClickNav._context;
 
-    if (!app3d.renderer3d._viewTravelling && intersected && infoMap[intersected.name]) {
-        INTERSECTED = infoMap[intersected.name];
-        winInfo.show(INTERSECTED.info, false);
-    } else {
-        if (INTERSECTED) {
-            winInfo.hide(); 
-            INTERSECTED = null;  
-        }
+        rightClickNav._hoveredObj = intersected.name || "";
+
+        markersNavMod.isMeshAMarker(intersected) ?
+            rightClickNav.setContext("markers", false) 
+            : rightClickNav.setContext("", rcContext === "markers");
     }
 }
 
-function onClick(ev) {
-    let intersected = ev.intersected,
-        infoMap = window.config.infoMap;
 
-    if (intersected && intersected.name.indexOf("arrow_") === 0) {
+function onClick(ev) {
+    let intersected = ev.intersected;
+
+    if (markersNavMod.isMeshAMarker(intersected)) {
         let ev = __d__.addEventDsptchr("markerOnPreShowFeedback");
-        ev.markerId = intersected.name.replace("arrow_", "");
-        app3d._baseNode.dispatchEvent(ev);	        
+        ev.markerId = intersected.name.replace(prefixMarker, "");
+        app3d._baseNode.dispatchEvent(ev);          
         return;
     }
+}
 
-    if (intersected && infoMap[intersected.name]) {
-        INTERSECTED = infoMap[intersected.name];
-        winInfo.show(INTERSECTED.info, true, intersected.name);
-    } else {
-        if (INTERSECTED) {
-            winInfo.hide(true); 
-            INTERSECTED = null;  
-        }
-    }
+function onObjectDoubleClick(ev) {
+    let intersected = ev.intersected,
+        name, obj;
+
+    if (!intersected) { return; }
+    name = intersected.name;
+
+    obj = app3d.groupsFactory.objs[name];
+
+    if (!obj || !obj.onGroups || !obj.onGroups.length ) { return; }
+
+    for(let j = 0, lenJ = obj.onGroups.length; j < lenJ; j += 1) {
+        if (!viewsNav.defaultsForInfowinId[obj.onGroups[j]]) { continue; }
+        viewsNav._goToDefaultInfowinFor(obj.onGroups[j]);
+        break;
+    }   
 }
 
 function keyPressed (ev) {
-    if (winInfo._layoverInfo.isVisible) { return; }
+    if (winInfo.iframeModule.visible) { return; }
 
-    if (mgmInfo.isVisible && ev.keyCode !== 77 && !ev.ctrlKey) { return; }
+    if (mgmInfo.isVisible) {
+        if ((ev.keyCode === 77 || ev.keyCode === 78) && (ev.ctrlKey || ev.altKey)) {
+            mgmInfo.toggle();
+        }
+        return;
+    } 
 
-    switch(ev.keyCode) {
-        case 27: // Esc key
-            viewsNav.goToViewId(null);
-            break;
-        case 16: // Shift key (alone)
-            app3d.glowSiblingShow();
-            break;
-        case 77: // Ctrl-M
-            if (!ev.ctrlKey || !mgmInfo.layv) { return; }
-            if (viewsNav.currentInfoWindowParentType && viewsNav.currentInfoWindowParentId && (viewsNav.currentMLInfoWindow || viewsNav.currentInfoWindow)) {
-                document.location.hash = '#/infowins/' + viewsNav.currentInfoWindowParentType + '/' + viewsNav.currentInfoWindowParentId + '/' + (viewsNav.currentMLInfoWindow || viewsNav.currentInfoWindow)._id;
+    switch (ev.keyCode) {
+        case 16:
+            // Shift key (alone)
+            for (let key in app3d.groupsFactory.groups) {
+                app3d.groupsFactory.groups[key].highlight = true;
             }
+            break;
+        case 27:
+            // Esc key
+            //viewsNav.goToViewId(null);
+            break;
+        case 32:
+            if ((rightClickNav._isVisible || rightClickNav._dialogIsVisible || mgmInfo.isVisible || markersNav._isOpened) && !mediaNav.__isPlaying) { return; }
+            ev.preventDefault();
+            mediaNav._toggle();
+        case 38: // Ctrl-Up
+            if (!(ev.ctrlKey || ev.shiftKey)) { return; }
+            winInfo.showPrev();            
+            break;
+        case 40: //Ctrl-Down
+            if (!(ev.ctrlKey || ev.shiftKey)) { return; }
+            winInfo.showNext();
+            break;            
+        case 77: // Ctrl-M
+            if (!(ev.ctrlKey || ev.altKey) || !mgmInfo.layv) { return; }
+            let cifw = viewsNav.currentInfoWindow;
+            if (!mgmInfo.isVisible && cifw && cifw.parentType && cifw.parentId) {
+                document.location.hash = '#/infowins/' + cifw.parentType + '/' + cifw.parentId + '/' + cifw._id;
+            }
+            mgmInfo.toggle();
+            break;            
+        case 78: // Ctrl-N
+            if (!(ev.ctrlKey || ev.altKey) || !mgmInfo.layv) { return; }
             mgmInfo.toggle();
             break;
         case 84: // Ctrl-T
-            if (!ev.ctrlKey || !markersNav.__markerUIMounted || markersNav._isOpened) { return; }
+            if (!(ev.ctrlKey || ev.altKey) || !markersNav.__markerUIMounted || markersNav._isOpened) { return; }
             markersNav._open(null);
-            break;
-        default: // Others
-            if ((markersNav && markersNav._isOpened) || (mgmInfo && mgmInfo.isVisible)) { return; } //don't move camera when editing!
-            let vw = viewsNav.viewsKeycodes[ev.keyCode];
-            if (vw) {
-                viewsNav.goToViewId(vw._id, true);
-            }                              
+            break;               
     }
 }
 
 function keyUp (ev) {
-    if (winInfo._layoverInfo.isVisible) { return; }
+    if (winInfo.iframeModule.visible) { return; }
     
     switch(ev.keyCode) {
         case 16:
-            app3d.glowSiblingHide();
+            for (let key in app3d.groupsFactory.groups) {
+                app3d.groupsFactory.groups[key].highlight = false;
+            }
             break;
     }
 }
 
 function onViewChanged (ev) {
-    let vw = ev.view, 
-        info,
-        infoMap = window.config.infoMap;
-
-    if (opxl && opxl.onViewChanged && typeof opxl.onViewChanged === 'function') {
-        opxl.onViewChanged(ev);
-    }
-
-    viewsNav._changeCursor(vw._id, false, viewsNav.currentMLInfoWindow);
+    let vw = ev.view, target;
 
     if (vw.highlighted) {
-        app3d.highlightGlowSibling(vw.highlighted);
-    }
-    
-    if (!vw.selected) {
-
-        //if Explore section is selected
-        if (viewsNav.node.querySelector('#viewer-nav .nav-tabs .active').innerText.includes('Explore')) {
-            winInfo.show({
-                title: vw.title,
-                html: vw.html,
-                id: vw._id
-            }, true);
-        }
-        else {
-            if (viewsNav.currentMLInfoWindow) {
-                winInfo.show({
-                    title: viewsNav.currentMLInfoWindow.title,
-                    html: viewsNav.currentMLInfoWindow.html,
-                    id: vw._id
-                }, true);    
-            }
-            else if (viewsNav.currentInfoWindow) {
-                winInfo.show({
-                    title: viewsNav.currentInfoWindow.title,
-                    html: viewsNav.currentInfoWindow.html,
-                    id: vw._id
-                }, true);
-            }
-            
-        }
-
-        return;
+        target = app3d.groupsFactory.get(vw.highlighted);
+        if (target) { target.flash = true; }
     }
 
-    info = infoMap[vw.selected];
-    if (!info) { return; }
-
-    INTERSECTED = info.obj;
-    winInfo.show(info.info, true, vw.selected);
-}
-
-function onViewChanging (ev) {
-    winInfo.hide(true);
-    if (opxl && opxl.onViewChanging && typeof opxl.onViewChanging === 'function') {
-        opxl.onViewChanging(ev);
+    if (vw.selected) {
+        let info = window.config.infoMap[vw.selected];
+        if (!info) { return; }
+        INTERSECTED = info.obj;
+        if (!info.info) { return; }
+        winInfo.show(info.info, true);
     }
 }
 
@@ -166,39 +166,43 @@ function onViewChanging (ev) {
 function initialized (ev) {
     console.info("opXL 3D-Viewer initialized!");
 
-    //__d__.addEventLnr(node, "pointerchanged", onHover);
-    //__d__.addEventLnr(node, "clicked", onClick);
-    //__d__.addEventLnr(node, "viewchanging", onViewChanging);
-    //__d__.addEventLnr(node, "viewchanged", onViewChanged);
+    __d__.addEventLnr(node, "pointerchanged", onHover);
+    __d__.addEventLnr(node, "clicked", onClick);
+    __d__.addEventLnr(node, "doubleclicked", onObjectDoubleClick.bind(scope));
+
+    __d__.addEventLnr(node, "viewchanged", onViewChanged);
 
     __d__.addEventLnr(window, "keydown", keyPressed.bind(scope));
-    __d__.addEventLnr(window, "keyup", keyUp.bind(scope));
+    __d__.addEventLnr(window, "keyup", keyUp.bind(scope));    
 
-
-    //__d__.addEventLnr(node, "markerOnShowFeedback", function(ev){
-    //    console.log("markerOnShowFeedback", ev);
-    //});
-
-    //__d__.addEventLnr(node, "markerOnDismissFeedback", function(ev){
-    //    console.log("markerOnDismissFeedback", ev);
-    //});
+    //__d__.addEventLnr(node, "markerOnShowFeedback", (ev) => { console.log(ev); });
+    //__d__.addEventLnr(node, "markerOnDismissFeedback", (ev) => { console.log(ev); });
 
 }
 
 /* Main events */
-window.location.hash = "";
+if (window.location.hash.indexOf(mainNavigatorMod.HASHBASE) !== 0) { 
+    window.location.hash = "";
+} else {
+    app3d.__initialInfowin = window.location.hash.replace(mainNavigatorMod.HASHBASE, "");
+}
 
 // Start the modules
 
 // 1. Views Navigator - can be used with window.app3dViewsNav
-viewsNav = new viewsNavMod.ViewsNav(app3d);
+viewsNav = new mainNavigatorMod.mainNavigator(app3d);
 // 2. Info Window - can be used with window.app3dInfowWindow
 winInfo  = new winInfoMod.WinInfo(app3d, viewsNav, window.opxl);
 // 3. Mgmt UI Window control - no need to expose it.
-mgmInfo  = new mgmInfoMod.MgmInfo(document.getElementById("mgmt-layv"), 
-                                    viewsNav, window.__reactApp, app3d, winInfo);
+mgmInfo  = new mgmInfoMod.MgmInfo(document.getElementById("mgmt-layv"), viewsNav, window.__reactApp, app3d, winInfo);
 // 4. MarkersNav controls
 markersNav = new markersNavMod.MarkersNav(viewsNav, window.__reactApp, app3d, winInfo, mgmInfo);                       
+// 5. Media Navigator
+mediaNav = new mediaNavMod.MediaNav(app3d, mgmInfo, viewsNav, winInfo);
+// 6. Right-click helpers
+rightClickNav = new rightClickMod.RightClick(app3d, viewsNav, winInfo, mgmInfo, window.__reactApp);
+// 7. Groups Commands
+gpsCmds  = new groupsCommandsMod.GroupsCmds(app3d, viewsNav, winInfo);
 
 __d__.addEventLnr(node, "initialized", initialized.bind(scope));
 
@@ -208,4 +212,7 @@ window.app3dInfowWindow = winInfo;
 window.app3dViewsNav = viewsNav;
 window.app3dMarkersNav = markersNav;
 window.app3dMgmInfo = mgmInfo;
-
+window.app3dMediaNav = mediaNav;
+window.app3dRightClickNav = rightClickNav;
+window.app3dGroupsCommands = gpsCmds;
+window.app3dTracker = appTracker;
